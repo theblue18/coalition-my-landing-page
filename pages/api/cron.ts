c6c9451505037dc6be871437f1cc715427000311
Hung Blue <theblue18@gmail.com>
2025-03-14 14:28:44 +0700

    update cron

---
+++ pages/api/cron.ts
@@ -0,0 +1,12 @@
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  console.log("🔄 Cron job executed at", new Date().toISOString());
+
+  res.status(200).json({ message: "Cron job executed successfully!" });
+}
